231f7cd165ab080d3fd115606bfe089a9b23231b
ErnestasFil <100300400+ErnestasFil@users.noreply.github.com>
2022-04-21 03:58:08 +0300

    Project

--- client/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Home } from "./home";
-export { default as Login } from "./login";
-export { default as Forgot } from "./forgot-password";
-export { default as Navigation } from "./navigation";
-export { default as Register } from "./register";
-export { default as Play } from "./play";
-export { default as Logout } from "./logout";
